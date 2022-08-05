2ca0ee541f38a2d8b71ac293ac8b954ed30abcb9
seba.brennan <brennan.seba@gmail.com>
2022-08-05 12:05:06 -0300

    firebase I

--- src/components/ItemCount.jsx
+++ src/components/ItemCount.jsx
@@ -1,38 +1,6 @@
-/* import React, { useState } from 'react';
-
-const ItemCount = ({initial, stock}) => {
-
-    const [count, setCount] = useState(initial);
-
-    const handleAdd = () => {
-        if (count < stock) setCount(prev => prev + 1)
-    }
-
-    const handleSubstract = () => {		
-        if (count > 1  && stock > 1) setCount(prev => prev - 1)
-    }
-
-    return ( 
-        <>
-            <div className="">
-                <div className=" ">
-                    <div className="">
-                        <div>
-                            <button className="btn" onClick={handleSubstract}>-</button>
-                            <button className="btn" onClick={handleAdd}>+</button>
-                            <span>{count}</span>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </>
-    );
-}
- 
-export default ItemCount; */
 
 import { useState } from "react";
-// Ponemos valores default a las props, ya que no funcionaría si nos olvidamos de enviarlas.
+
 const ItemCount = ({ stock = 2, initial = 0, onAdd }) => {
   const [count, setCount] = useState(initial);
   const updateCount = (operator) => {
--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,33 +1,41 @@
 import { useState, useEffect } from "react";
-import database from "./helpers/database";
 import ItemList from "./ItemList";
 import { useParams } from "react-router-dom"
 import Spinner from "./helpers/spinner"
 import {
-    getDoc,
-    doc,
     getFirestore,
     collection,
     getDocs,
     query,
     where,
-    limit,
   } from "firebase/firestore";
 
 const ItemListContainer = () => {
 
+   /*  const params = useParams()
+    console.log(params) */
+    
+    const {categoryId} = useParams();
+    console.log(categoryId)
+
     const [items, setItems] = useState([]);
     const [loading, setLoading] = useState(true)
 
-    useEffect( () => {
+    const container = () => {
         const db = getFirestore();
         const itemsCollection = collection(db, "items");
-        getDocs (itemsCollection).then(snapshot => {
+        const fillterCollection = categoryId ? query(itemsCollection, where("category", "==", categoryId))
+        : itemsCollection;
+        getDocs (fillterCollection).then(snapshot => {
             const data = snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
             setItems(data);
             setLoading(false)
         })
-    })
+    }
+
+    useEffect( () => {
+        container();
+    }, [categoryId])
 
     return (
         <> 
